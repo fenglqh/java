ec3f4a2423ef673c6be9335c9f435a14c5245229
fenglqh <1691141895@qq.com>
2023-05-23 23:34:45 +0800

    汉诺塔问题

---
+++ J2023_5_23/src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    /**
+     * 解决汉诺塔问题
+     * @param args
+     */
+    public static void move(int n, char pos1, char pos2, char pos3) {
+        if(n == 1) {
+            System.out.println(pos1 +"->" +pos2);
+        } else {
+            
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
