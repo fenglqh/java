188e97950761f6e2acdf84b864011a69d7bce444
flqh <1691141895@qq.com>
2023-10-22 08:47:48 +0800

    test

---
+++ J2023_6_11/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    /**
+     * 复习累了来巩固一下数组相关的知识
+     */
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a;
+        int[] arr1 = new int[n];
+    }
+//    Scanner sc = new Scanner(System.in);
+//    public static void main(String[] args) {
+//        int n = sc.nextInt();
+//        int[] arr1 = new int[2];
+//    }//没搞懂为啥错了，学完类和对象回来看看。
+}
