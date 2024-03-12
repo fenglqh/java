4848532f265340746d40e9dbd1889acce21097f9
flqh <1691141895@qq.com>
2024-03-12 23:52:18 +0800

    小练习

--- J2024_3_11sort2/src/Main.java
+++ J2024_3_11sort2/src/Main.java
@@ -1,4 +1,25 @@
 public class Main {
+//    LeetCode611 暴力解法
+    public static int triangleNumber(int[] nums) {
+    int sum = 0;
+    for (int i = 0; i < nums.length-2; i++) {
+        for (int j = i + 1; j < nums.length-1; j++) {
+            for (int k = j+1; k < nums.length; k++) {
+                if (nums[i] + nums[j] > nums[k] &&
+                        nums[i] + nums[k] > nums[j] && nums[j] + nums[k] > nums[i]) {
+                    sum += 1;
+                }
+            }
+
+        }
+    }
+    return sum;
+}
+
+    public static void main(String[] args) {
+        int arr[] = {2,2,3,4};
+        System.out.println(triangleNumber(arr));
+    }
     //leetcode 34用两个二分查找
     public int[] searchRange(int[] nums, int target) {
         int left = 0, right = nums.length-1;
@@ -62,7 +83,7 @@ public class Main {
     private static int minN(int a, int b) {
         return a > b ? b : a;
     }
-    public static void main(String[] args) {
+    public static void main4(String[] args) {
         int[] arr = {1,8,6,2,5,4,8,3,7};
         maxArea(arr);
     }
