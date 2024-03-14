71bf18bf6bd87a3975a54ec4c957f91d6a8c37cc
flqh <1691141895@qq.com>
2024-03-15 00:31:06 +0800

    oj

--- J2024_3_11sort2/src/Main.java
+++ J2024_3_11sort2/src/Main.java
@@ -1,4 +1,85 @@
+import java.util.*;
+
 public class Main {
+
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> back = new ArrayList<>();
+        Arrays.sort(nums);
+        int i = 0;
+        while(i < nums.length -2) {
+            if (nums[i] > 0) {
+                break;//还可以优化，当i是正数时，在后面是找不到小于0的数的
+            }
+            int target = -nums[i];
+            int left = i+1, right = nums.length-1;
+            while(left < right) {
+                int sum = nums[left] + nums[right];
+                if(sum > target) {
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    List<Integer> ret = new ArrayList<>();
+                    ret.add(nums[i]);
+                    ret.add(nums[left]);
+                    ret.add(nums[right]);
+
+                    back.add(ret);
+//                    int tmp = nums[left++];
+//                    while(left < right && tmp == nums[left]) left++;//注意点，小范围循环，还要在注意一下范围
+                    left++;
+                    while(left < right && nums[left] == nums[left-1]) left++;//注意点，小范围循环，还要在注意一下范围
+                    right--;
+                    while(left < right && nums[right] == nums[right+1]) right--;//细节，比较的时候要和上一个数比，而不是和下一个数比
+                }
+            }
+//            int tmp = nums[i];
+
+//            while(i < nums.length-1 &&  nums[i+1] == nums[i]) i++;
+//            i++;
+            i++;
+            while(i < nums.length-1 && nums[i] == nums[i-1]) i++;
+        }
+        return back;
+    }
+    public static void main(String[] args) {
+        int[] arr = {-1,0,1,2,-1,-4};
+        System.out.println(threeSum(arr));
+    }
+    public static ArrayList<Integer> FindNumbersWithSum(int [] array,int sum) {
+        int left = 0;
+        int right = array.length-1;
+        ArrayList<Integer> ret = new ArrayList<>();
+        while(left < right) {
+            int tmp = array[left] + array[right];
+            if(tmp > sum) {
+                right--;
+            } else if(tmp < sum) {
+                left++;
+            } else{
+                ret.add(array[left]);
+                ret.add(array[right]);
+                break;
+            }
+        }
+
+
+        return ret;
+    }
+
+    public static void main6(String[] args) {
+        int[] arr = {1,2,4,7,11,15};
+//        ArrayList<Integer> a = FindNumbersWithSum(arr, 15);
+//        System.out.println(a);
+        //突然想到一个点 数组能比较吗
+        int[] arr1 = {-1,0,1};
+        int[] arr2 = {-1,0,1};
+        System.out.println(arr1 == arr2);//false
+        System.out.println(Arrays.equals(arr1, arr2));//true
+        int[] arr3 = {-1,1,0};
+        System.out.println(Arrays.equals(arr3, arr1));//false
+    }
 //    LeetCode611 暴力解法
     public static int triangleNumber(int[] nums) {
     int sum = 0;
@@ -13,10 +94,11 @@ public class Main {
 
         }
     }
+
     return sum;
 }
 
-    public static void main(String[] args) {
+    public static void main5(String[] args) {
         int arr[] = {2,2,3,4};
         System.out.println(triangleNumber(arr));
     }
