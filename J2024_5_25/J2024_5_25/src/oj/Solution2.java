b942fb24e39c0e4ec8ff1a0bb98c531eb0f2623a
flqh <1691141895@qq.com>
2024-05-26 23:19:07 +0800

    test

---
+++ J2024_5_25/J2024_5_25/src/oj/Solution2.java
@@ -0,0 +1,21 @@
+package oj;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        HashSet<Integer> hs = new HashSet<>();
+        hs.add(1);
+        boolean contains = hs.contains(1);
+        System.out.println(contains);
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        hm.put(1, 2);
+        hm.containsKey(1);
+        hm.get()
+
+    }
+    public int[] test() {
+        return new int[]{1,2,3,4,5,6,7,8,9};
+    }
+}
