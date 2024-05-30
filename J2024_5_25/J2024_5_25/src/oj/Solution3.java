0779a0011117a8336b0e03300f4bd9cb00d9a95e
flqh <1691141895@qq.com>
2024-05-30 21:59:46 +0800

    oj

--- J2024_5_25/J2024_5_25/src/oj/Solution3.java
+++ J2024_5_25/J2024_5_25/src/oj/Solution3.java
@@ -4,6 +4,33 @@ import java.util.Arrays;
 
 public class Solution3 {
     public static void main(String[] args) {
+        String a = "babad";
+//        String ret = a.substring(0,2);// 左闭右开
+//        System.out.println(ret);
+        System.out.println(longestPalindrome(a));
+    }
+    public static String longestPalindrome(String s) {
+//      1. 中心扩展算法
+        int len = 0;
+        String ret = null;
+        for (int i = 0; i < s.length(); i++) {
+            int right = i, left = i;
+            while (right < s.length() && left >= 0 && s.charAt(right) == s.charAt(left)) {
+                right++;
+                left--;
+            }
+            if ((right - left - 1) > len) {
+                ret = s.substring(++left,right);
+                len = right - left - 1;
+            }
+            left = i;
+            right = i+1;
+
+
+        }
+        return ret;
+    }
+    public static void main1(String[] args) {
         String[] str = {"flower","flow","flight"};
         Arrays.sort(str);
         System.out.println(longestCommonPrefix(str));
