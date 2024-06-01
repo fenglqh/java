df1da0ecb601e29358f1a4f06e8239f336dbb715
flqh <1691141895@qq.com>
2024-06-01 22:22:11 +0800

    oj

--- J2024_5_25/J2024_5_25/src/oj/Solution3.java
+++ J2024_5_25/J2024_5_25/src/oj/Solution3.java
@@ -1,9 +1,57 @@
 package oj;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
 
 public class Solution3 {
+
+    public static void main3(String[] args) {
+        String a = "11";
+        String b = "1";
+        System.out.println(addBinary(a, b));
+    }
+    public static String addBinary(String a, String b) {
+        // 高精度求和 本质是模拟
+        Integer aa = Integer.valueOf(a);
+        Integer bb = Integer.valueOf(b);
+        Stack<Integer> st = new Stack<>();
+        int tmp = 0;
+        while (aa != 0 || bb != 0) {
+            tmp += (aa % 2 + bb % 2);
+            st.push(tmp % 2);
+            tmp = tmp / 2;
+            if (aa == 1) aa = 0;
+            else aa = aa / 10;
+            if (bb == 1) bb = 0;
+            else bb = bb / 10;
+        }
+        if (tmp != 0) st.push(tmp);
+
+        String ret = "";
+        while (!st.isEmpty()) ret += st.pop();
+
+        return ret;
+
+    }
     public static void main(String[] args) {
+        String a = "33";
+        int c = a.charAt(0) - '0';
+        int d = a.charAt(1) - '0';
+
+        System.out.println(c);
+        System.out.println(d);
+        int s = c + d;
+        System.out.println(s);
+//        Integer i = Integer.valueOf(a);
+//        List<Integer> list = Arrays.asList(i);
+//        System.out.println(list);
+//        Stack stack = new Stack();
+//        stack.push(i);
+//        stack.pop();
+
+    }
+    public static void main2(String[] args) {
         String a = "babad";
 //        String ret = a.substring(0,2);// 左闭右开
 //        System.out.println(ret);
