452f0d129ada9c16ef5a8c3fc603331121b1923c
flqh <1691141895@qq.com>
2024-06-06 22:54:49 +0800

    test

---
+++ J2024_5_25/J2024_5_25/src/selenuim/Main.java
@@ -0,0 +1,8 @@
+package selenuim;
+
+public class Main {
+    public static void main(String[] args) {
+//        WebDriver webDriver = new ChromeDriver();
+//        webDriver.get("https://www.baidu.com");
+    }
+}
